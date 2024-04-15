3ea39a730d0e94287b3a18847c95417d8dc9de07
rundongzhong <ryandzhong@gmail.com>
2024-04-14 22:01:37 -0400

    finished setup auth functionality and deployment

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import axios from "axios";
+
+let token = localStorage.getItem("token");
+if (token) {
+  console.log(token);
+  axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+}
 
 const app = createApp(App);
 
 app.use(router);
 
-app.mount('#app');
+app.mount("#app");
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,94 +1,125 @@
 <template>
-    <div class="login-container">
-      <h1>Login</h1>
-      <form @submit.prevent="login">
-        <!-- 登录表单输入 -->
-        <input type="text" placeholder="Username" v-model="username">
-        <input type="password" placeholder="Password" v-model="password">
-        <!-- 登录按钮 -->
-        <button type="submit">Login</button>
-      </form>
-      <!-- 游客登录链接 -->
-      <router-link to="/" class="auth-link guest-login-btn">Guest Login</router-link>
-      <!-- 注册页面链接 -->
-      <router-link to="/register" class="auth-link register-btn">Register</router-link>
+  <div class="login-container">
+    <h1>Login</h1>
+    <div class="login_form">
+      <!-- 登录表单输入 -->
+      <input type="text" placeholder="Username" v-model="username" />
+      <input type="password" placeholder="Password" v-model="password" />
+      <!-- 登录按钮 -->
+      <button @click="loginUser">Login</button>
+      <RouterLink to="/register" class="tosignup">
+        Click here to Sign Up</RouterLink
+      >
     </div>
-  </template>
-  
-  <script>
-  export default {
-    // eslint-disable-next-line vue/multi-word-component-names
-    name: 'Login',
-    data() {
-      return {
-        username: '',
-        password: ''
-      }
+    <!-- 游客登录链接 -->
+    <!-- <router-link to="/" class="auth-link guest-login-btn">Guest Login</router-link> -->
+    <!-- 注册页面链接 -->
+    <!-- <router-link to="/register" class="auth-link register-btn">Register</router-link> -->
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Login",
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    loginUser() {
+      // 执行登录逻辑
+      const instance = axios.create();
+      const requestData = {
+        username: this.username,
+        password: this.password,
+      };
+
+      instance
+        .post("api/user/login", requestData, {
+          headers: {
+            Authorization: null,
+          },
+        })
+        .then((response) => {
+          console.log(response);
+          if (response.statusText === "OK" || response.status === 200) {
+            const token = JSON.stringify(response.data.token);
+            localStorage.setItem("token", token);
+            axios.defaults.headers.common["Authorization"] =
+              "Bearer " + JSON.parse(token);
+            //如果登录成功，重定向到主页
+            this.$router.push("/");
+          }
+          this.userData = response.data;
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          this.username = "";
+          this.password = "";
+        });
     },
-    methods: {
-      login() {
-        // 执行登录逻辑
-        // 如果登录成功，重定向到主页
-        this.$router.push('/');
-      }
-    }
-  }
-  </script>
-  
-  <style scoped>
-  .login-container {
-    max-width: 300px;
-    margin: 40px auto;
-    padding: 20px;
-    text-align: center;
-    background: #fff;
-    border-radius: 10px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  }
-  
-  .login-container h1 {
-    margin-bottom: 20px;
-    color: #333;
-  }
-  
-  .login-container form {
-    display: flex;
-    flex-direction: column;
-  }
-  
-  .login-container input {
-    margin-bottom: 10px;
-    padding: 10px;
-    border: 1px solid #ddd;
-    border-radius: 5px;
-    box-sizing: border-box;
-  }
-  
-  .login-container button,
-  .auth-link {
-    padding: 10px;
-    margin-top: 10px;
-    border: none;
-    border-radius: 5px;
-    background-color: #5cb85c;
-    color: white;
-    cursor: pointer;
-    font-size: 1rem; /* 字号调整 */
-    text-decoration: none;
-    display: block;
-  }
-  
-  .login-container button:hover {
-    background-color: #4cae4c;
-  }
-  
-  .auth-link {
-    background-color: #5cb85c;
-    text-align: center;
-  }
-  
-  .auth-link:hover {
-    background-color: #5cb85c;
-  }
-  </style>
-  
+  },
+};
+</script>
+
+<style scoped>
+.login-container {
+  max-width: 300px;
+  margin: 40px auto;
+  padding: 20px;
+  text-align: center;
+  background: #fff;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.login-container h1 {
+  margin-bottom: 20px;
+  color: #333;
+}
+
+.login_form {
+  display: flex;
+  flex-direction: column;
+}
+
+.login-container input {
+  margin-bottom: 10px;
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  box-sizing: border-box;
+}
+
+.login-container button,
+.auth-link {
+  padding: 10px;
+  margin-top: 10px;
+  border: none;
+  border-radius: 5px;
+  background-color: #5cb85c;
+  color: white;
+  cursor: pointer;
+  font-size: 1rem; /* 字号调整 */
+  text-decoration: none;
+  display: block;
+}
+
+.login-container button:hover {
+  background-color: #4cae4c;
+}
+
+.auth-link {
+  background-color: #5cb85c;
+  text-align: center;
+}
+
+.auth-link:hover {
+  background-color: #5cb85c;
+}
+</style>
